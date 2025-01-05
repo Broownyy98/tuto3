2f8db420b500a2f9c6fef97f3a6c9bae33296012
Bennai1998@gmail.com <166769551+Broownyy98@users.noreply.github.com>
2025-01-05 17:11:47 +0100

    creating the Credentials and test them

--- app/src/test/java/com/example/tuto3/CredentialsManagerTest.kt
+++ app/src/test/java/com/example/tuto3/CredentialsManagerTest.kt
@@ -1,42 +1,38 @@
-// Put your package name here. Check your activity for reference.
 package com.example.tuto3
-
-import junit.framework.TestCase.assertTrue
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
 
 class CredentialsManagerTest {
     @Test
     fun givenEmptyEmail_thenReturnFalse() {
         val credentialsManager = CredentialsManager()
         val isEmailValid = credentialsManager.isEmailValid("")
-        Assert.assertEquals(false, isEmailValid)
+        assertEquals(false , isEmailValid)
     }
 
     @Test
     fun givenProperEmail_thenReturnTrue() {
         val credentialsManager = CredentialsManager()
         val isEmailValid = credentialsManager.isEmailValid("email@example.com")
-        Assert.assertEquals(true, isEmailValid)
+        assertEquals(true, isEmailValid)
     }
 
     @Test
     fun givenWrongEmailFormat_thenReturnFalse() {
         val credentialsManager = CredentialsManager()
         val isEmailValid = credentialsManager.isEmailValid("email@.st")
-        Assert.assertEquals(false, isEmailValid)
+        assertEquals(false, isEmailValid)
     }
 
     @Test
     fun givenEmptyPassword_thenReturnFalse() {
         val credentialsManager = CredentialsManager()
-        Assert.assertEquals(false, credentialsManager.isPasswordValid(""))
+        assertEquals(false, credentialsManager.isPasswordValid(""))
     }
 
     @Test
     fun givenValidPassword_thenReturnTrue() {
         val credentialsManager = CredentialsManager()
-        Assert.assertEquals(true, credentialsManager.isPasswordValid("StrongP@ssw0rd"))
+        assertEquals(true, credentialsManager.isPasswordValid("StrongP@ssw0rd"))
     }
 }
