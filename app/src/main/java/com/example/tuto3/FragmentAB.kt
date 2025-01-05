7d04b2aa4580d3bd12f6431ff1d18436401fcd76
Bennai1998@gmail.com <166769551+Broownyy98@users.noreply.github.com>
2025-01-05 19:45:55 +0100

    adding fragment samples A and B

---
+++ app/src/main/java/com/example/tuto3/FragmentAB.kt
@@ -0,0 +1,38 @@
+package com.example.tuto3
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+
+class FragmentAB : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.fragment_a__b)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        findViewById<View>(R.id.switch_button).setOnClickListener{
+
+            val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view)
+
+            supportFragmentManager.commit {
+
+                if(currentFragment is FragmentA)
+                {
+                    replace<FragmentB>(R.id.fragment_container_view)
+                    addToBackStack(null)
+                }else
+                {
+                    supportFragmentManager.popBackStack()
+                }
+            }
+        }
+    }
+}
